6c37a807b5f8ce50c54e0c3f4222a7f9bcbea9cc
leroyardonia <lardonia@q90.com>
2024-11-25 00:13:36 +1100

    Created command. Added prompts.

---
+++ app/Commands/RunCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+use function Termwind\render;
+
+class RunCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run report generator for a given student.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        render("Please enter the following");
+        $studentId = $this->ask('Student ID: ');
+        if (!empty($studentId)) {
+            $student = new \App\Actions\GetStudent($studentId);
+            if (!$student->execute()) {
+                render("Invalid student ID. Please try again.");
+                return;
+            }
+        }
+        else {
+            render("You need to enter a Student ID. Please try again.");
+            return;
+        }
+
+        $reportId = $this->ask('Report to generate (1 for Diagnostic, 2 for Progress, 3 for Feedback): ');
+
+        if (empty($reportId)) {
+            render("You need to enter a Report ID. Please try again.");
+            return;
+        }
+
+        switch ($reportId) {
+            case '1':
+                $diagnostic = new \App\Actions\GenerateDiagnosticReport($studentId);
+                $diagnostic->execute();
+                break;
+            case '2':
+                $progress = new \App\Actions\GenerateProgressReport($studentId);
+                $progress->execute();
+                break;
+            case '3':
+                $feedback = new \App\Actions\GenerateFeedbackReport($studentId);
+                $feedback->execute();
+                break;
+            
+            default:
+                render("Invalid option. Please try again.");
+                break;
+        }
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
